1da3c27b4e61b34906444dfaa3a4d8c7bf8c63ee
Lainter <69177753+Lainter@users.noreply.github.com>
2025-10-29 16:50:38 +0100

    Delete Test2 directory

--- Test2/A1/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.home, name="home"),
-    path("get-question/", views.get_question, name="get_question"),
-    path("check-answer/", views.check_answer, name="check_answer"),
-]
